1f8f70759d88ba28abc91d60e75d4d168ed8a9b5
Otto Jones <ottosjonesjr@gmail.com>
2022-05-19 22:25:27 +0300

    added car form component and imported form into edit car details and car registration componenets.

---
+++ src/components/sub_components/CarForm.jsx
@@ -0,0 +1,326 @@
+import React from 'react'
+import { upperCase, numberWithCommas } from '../../utilities.js/functions'
+
+function CarForm({ onChange, carDetails }) {
+  let {
+    _id,
+    make,
+    year,
+    model,
+    type,
+    listprice,
+    color,
+    drivetype,
+    engine,
+    transmission,
+    discription,
+    image,
+    mileage,
+    ac,
+    leatherseats,
+    sunroof,
+    bluetooth,
+    // comments,
+    cruisecontrol,
+    satradio,
+    auxport,
+    amfm,
+  } = carDetails
+
+  return (
+    <>
+      <div className="col-6">
+        <label htmlFor="make" className="form-label">
+          Make
+        </label>
+        <input
+          defaultValue={upperCase(make)}
+          autoFocus
+          type="text"
+          className="form-control"
+          name="make"
+          id="make"
+          placeholder="Make"
+          onChange={onChange}
+        />
+      </div>
+      <div className="col-6">
+        <label htmlFor="model" className="form-label">
+          Model
+        </label>
+        <input
+          defaultValue={upperCase(model)}
+          type="text"
+          className="form-control"
+          name="model"
+          id="model"
+          placeholder="Model"
+          onChange={onChange}
+        />
+      </div>
+
+      <div className="col-6">
+        <label htmlFor="year" className="form-label">
+          Year
+        </label>
+        <input
+          defaultValue={year}
+          type="number"
+          className="form-control"
+          name="year"
+          id="year"
+          onChange={onChange}
+        />
+      </div>
+      <div className="col-6">
+        <label htmlFor="type" className="form-label">
+          Body Type
+        </label>
+        <input
+          defaultValue={upperCase(type)}
+          type="text"
+          className="form-control"
+          name="type"
+          id="type"
+          onChange={onChange}
+        />
+      </div>
+      <div className="col-6">
+        <label htmlFor="listprice" className="form-label">
+          Listprice
+        </label>
+        <input
+          defaultValue={`$${numberWithCommas(listprice)}`}
+          type="text"
+          className="form-control"
+          name="listprice"
+          id="listprice"
+          onChange={onChange}
+        />
+      </div>
+      <div className="col-6">
+        <label htmlFor="color" className="form-label">
+          Color
+        </label>
+        <input
+          defaultValue={upperCase(color)}
+          type="text"
+          className="form-control"
+          name="color"
+          id="color"
+          onChange={onChange}
+        />
+      </div>
+      <div className="col-6">
+        <label htmlFor="drivetype" className="form-label">
+          Drive Type
+        </label>
+        <input
+          defaultValue={upperCase(drivetype ? drivetype : 'N/A')}
+          type="text"
+          className="form-control"
+          name="drivetype"
+          id="drivetype"
+          placeholder=""
+          onChange={onChange}
+        />
+      </div>
+
+      <div className="col-6">
+        <label htmlFor="engine" className="form-label">
+          Engine
+        </label>
+        <input
+          defaultValue={upperCase(engine ? engine : 'N/A')}
+          type="text"
+          className="form-control"
+          name="engine"
+          id="engine"
+          onChange={onChange}
+        />
+      </div>
+
+      <div className="col-6">
+        <label htmlFor="transmission" className="form-label">
+          Transmission
+        </label>
+        <input
+          defaultValue={upperCase(transmission ? transmission : 'N/A')}
+          type="text"
+          className="form-control"
+          name="transmission"
+          id="transmission"
+          onChange={onChange}
+        />
+      </div>
+
+      <div className="col-6">
+        <label htmlFor="mileage" className="form-label">
+          Mileage
+        </label>
+        <input
+          defaultValue={numberWithCommas(mileage)}
+          type="text"
+          className="form-control"
+          name="mileage"
+          id="mileage"
+          onChange={onChange}
+        />
+      </div>
+
+      <div
+        className="col-12
+"
+      >
+        <label htmlFor="image" className="form-label ">
+          Image
+        </label>
+        <input
+          defaultValue={image}
+          type="text"
+          className="form-control"
+          name="image"
+          id="image"
+          onChange={onChange}
+        />
+      </div>
+
+      <div className="headings">
+        <h2> Additional Options</h2>
+      </div>
+      <div className="container">
+        <div className="options-container">
+          <div className="col-md-2">
+            <label htmlFor="ac" className="form-label">
+              A/C
+            </label>
+            <select
+              id="ac"
+              name="ac"
+              className="form-select select-box-size"
+              onChange={onChange}
+              defaultValue={ac}
+            >
+              <option value="Choose"> {ac ? 'Yes' : 'No'} </option>
+              <option value="true">Yes </option>
+              <option value="false">No </option>
+            </select>
+          </div>
+          <div className="col-md-2">
+            <label htmlFor="leatherseats" className="form-label">
+              Leather
+            </label>
+            <select
+              id="leatherseats"
+              name="leatherseats"
+              className="form-select select-box-size"
+              onChange={onChange}
+            >
+              <option value="Choose"> {leatherseats ? 'Yes' : 'No'} </option>
+
+              <option value="true">Yes</option>
+              <option value="false">No</option>
+            </select>
+          </div>
+          <div className="col-md-2">
+            <label htmlFor="sunroof" className="form-label">
+              Sun Roof
+            </label>
+            <select
+              id="sunroof"
+              name="sunroof"
+              className="form-select select-box-size"
+              onChange={onChange}
+            >
+              <option value="Choose"> {sunroof ? 'Yes' : 'No'} </option>
+
+              <option value="true">Yes</option>
+              <option value="false">No</option>
+            </select>
+          </div>
+          <div className="col-md-2">
+            <label htmlFor="bluetooth" className="form-label">
+              Bluetooth
+            </label>
+            <select
+              id="bluetooth"
+              name="bluetooth"
+              className="form-select select-box-size"
+              onChange={onChange}
+            >
+              <option value="Choose"> {bluetooth ? 'Yes' : 'No'} </option>
+
+              <option value="true">Yes </option>
+              <option value="false">No </option>
+            </select>
+          </div>
+          <div className="col-md-2">
+            <label htmlFor="cruisecontrol" className="form-label">
+              Cruise Control
+            </label>
+            <select
+              id="cruisecontrol"
+              name="cruisecontrol"
+              className="form-select select-box-size"
+              onChange={onChange}
+            >
+              <option value="Choose"> {cruisecontrol ? 'Yes' : 'No'} </option>
+
+              <option value="true">Yes </option>
+              <option value="false">No </option>
+            </select>
+          </div>
+          <div className="col-md-2 ">
+            <label htmlFor="satradio" className="form-label">
+              Sat Radio
+            </label>
+            <select
+              id="satradio"
+              name="satradio"
+              className="form-select select-box-size"
+              onChange={onChange}
+            >
+              <option value="Choose"> {satradio ? 'Yes' : 'No'} </option>
+
+              <option value="true">Yes </option>
+              <option value="false">No </option>
+            </select>
+          </div>
+          <div className="col-md-2 ">
+            <label htmlFor="auxport" className="form-label">
+              Aux Port
+            </label>
+            <select
+              id="auxport"
+              name="auxport"
+              className="form-select select-box-size"
+              onChange={onChange}
+            >
+              <option value="Choose"> {auxport ? 'Yes' : 'No'} </option>
+
+              <option value="true">Yes </option>
+              <option value="false">No </option>
+            </select>
+          </div>
+          <div className="col-md-2">
+            <label htmlFor="amfm" className="form-label">
+              Am/Fm
+            </label>
+            <select
+              id="amfm"
+              name="amfm"
+              className="form-select select-box-size"
+              onChange={onChange}
+            >
+              <option value="Choose"> {amfm ? 'Yes' : 'No'} </option>
+              <option value="true">Yes </option>
+              <option value="false">No </option>
+            </select>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default CarForm
